18423ed038eb707f9f2b4f0f24ec82f5a8d19de1
Joseph Bona <joseph.bona07@gmail.com>
2019-10-03 10:40:28 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import MovieCard from './MovieCard'
+import './App.css'
 
-function App() {
+const API_KEY = 'c2ecec45'
+const API_URL = 'https://www.omdbapi.com'
+const MOVIE_ID = 'tt0120623'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <MovieCard />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/MovieCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MovieCard.css'
+
+const MovieCard = () => {
+  return ( 
+    <div className='movieCard'>
+    </div>
+  )
+}
+
+export default MovieCard
