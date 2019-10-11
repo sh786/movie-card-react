3872f408f20a671307712d5a2d64b20cfc3b4ad4
Sam Hamburger <samhamburger@Sams-MBP-2.lan1>
2019-10-11 09:08:31 -0400

    Updates

---
+++ precache-manifest.25e7adc460b79944c8f8c81c01af2d17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6649772b7974f8393b475fde3113424f",
+    "url": "/movie-card-react/index.html"
+  },
+  {
+    "revision": "c39328d26c0509fef8bf",
+    "url": "/movie-card-react/static/css/main.62fec6ce.chunk.css"
+  },
+  {
+    "revision": "ecb54fda9ed00d3fecf8",
+    "url": "/movie-card-react/static/js/2.bd22e1cf.chunk.js"
+  },
+  {
+    "revision": "c39328d26c0509fef8bf",
+    "url": "/movie-card-react/static/js/main.68d2abe6.chunk.js"
+  },
+  {
+    "revision": "71e54f7f69dd162d3ece",
+    "url": "/movie-card-react/static/js/runtime-main.cc046b9e.js"
+  }
+]);
